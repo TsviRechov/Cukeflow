796bec2deb5f7dcb58c14d7ddd7ab96892c7a761
tsvi <tsvi@tsvi-laptop.(none)>
2012-11-11 08:38:56 -0800

    initial commit. full-bore cucumber tdd development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cukeflow::Application.config.secret_token = '411a3aa72fd6e0f58e4bb5aa07f4f87403ad40068f2e1d0daab96e7be8d560b756d75be1e9dbaee7e6d6a6c3cc483e90249937ab8d41639066a16bea00f3b073'
---
+++ features/all_items.feature
@@ -0,0 +1,10 @@
+Feature: user views all items in cashflow accountf
+	As a user
+	I want to be able to see the items in my cashflow account
+	in order to know when I am running out of money
+
+	Scenario:  user creates a cashflow account
+
+	Scenario:  user adds an income item to their cashflow account
+
+	Scenario:  user adds an expense item to their cashflow account
