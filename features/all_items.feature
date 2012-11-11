6143068a4303c5912c3b70044cedaf50f79ca195
tsvi <tsvi@tsvi-laptop.(none)>
2012-11-11 11:26:40 -0800

    created cucumber features and scenarios for view total income and expenses for a date range against a cashflow account

--- features/all_items.feature
+++ features/all_items.feature
@@ -4,7 +4,15 @@ Feature: user views all items in cashflow accountf
 	in order to know when I am running out of money
 
 	Scenario:  user creates a cashflow account
+		Given that I do not have a cashflow account
+		When I go to the homepage
+			And I follow "New cashflow account"
+			And I fill in "name" with "Tsvi Bar-David"
+			And I press "Create new cashflow account"
+		Then I see a cashflow account with the "name" "Tsvi Bar-David" on the showpage for a cashflow account
 
 	Scenario:  user adds an income item to their cashflow account
 
 	Scenario:  user adds an expense item to their cashflow account
+
+	Scenario:  user views all the items of their cashflow account
---
+++ features/step_definitions/cashflow_steps.rb
@@ -0,0 +1,23 @@
+Given /^that I do not have a cashflow account$/ do
+	Account.empty?.should == true
+end
+
+When /^I go to the homepage$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I follow "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in "(.*?)" with "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see a cashflow account with the "(.*?)" "(.*?)" on the showpage for a cashflow account$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
